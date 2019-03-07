bdba3fd5df5f7107cee7285e02e472c25e8c3028
Alex Puga <alpuga8@gmail.com>
2019-03-06 20:09:37 -0500

    connect to mongo

--- server.js
+++ server.js
@@ -1,56 +1,58 @@
-const express   = require('express');
+const express = require("express");
 let app = express();
 
-
-const bodyParser = require('body-parser');
-const mongoose  = require('mongoose');
-const http      = require('http').Server(app);
-const io        = require('socket.io')(http);
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 let port = process.env.PORT || 3000;
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-let Message = mongoose.model('Message', {
+let Message = mongoose.model("Message", {
   name: String,
   message: String
 });
 
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true}, (err, db) => {
-  if (err) {
-    console.log('Database error: ' + err);
-  } else {
-    console.log('Successful Database Connection')
+// connect to MongoDB
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true },
+  (err, db) => {
+    if (err) {
+      console.log("Database error: " + err);
+    } else {
+      console.log("Successful Database Connection");
+    }
   }
-});
+);
 
-app.get('/messages', (req, res) => {
+app.get("/messages", (req, res) => {
   Message.find({}, (err, messages) => {
     res.send(messages);
-  })
+  });
 });
 
-app.route('/messages')
-   .post((req, res) => {
-     let message = new Message(req.body);
-     message.save((err) => {
-     if (err) {
-       sendStatus(500);
-     }
-     io.emit('message', req.body);
-     res.sendStatus(200);
-  })
+app.route("/messages").post((req, res) => {
+  let message = new Message(req.body);
+  message.save(err => {
+    if (err) {
+      sendStatus(500);
+    }
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+io.on("connection", socket => {
+  console.log("a user connected");
 });
 
 let server = http.listen(port, () => {
-  console.log('Server is running on port', server.address().port);
+  console.log("Server is running on port", server.address().port);
 });
